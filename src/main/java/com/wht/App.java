2e470084f163936579b5c90a68aa23423e2692fa
wht <wht@qq.com>
2022-06-01 18:51:18 +0800

    hot-fix first commit

--- src/main/java/com/wht/App.java
+++ src/main/java/com/wht/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!1111" );
+        System.out.println( "Hello World!1111" );
     }
 }
