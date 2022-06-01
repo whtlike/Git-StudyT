8b76ace0e0e4ce9974ca865f162f9b7a24c8882a
whtlike <106653555+whtlike@users.noreply.github.com>
2022-06-01 19:39:58 +0800

    Pull
    Pull

--- src/main/java/com/wht/App.java
+++ src/main/java/com/wht/App.java
@@ -13,5 +13,6 @@ public class App
         System.out.println( "Hello World!1222221" );
         System.out.println( "Hello World!1111" );
         System.out.println( "push World!1111" );
+         System.out.println( "pull World!1111" );
     }
 }
